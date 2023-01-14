c7722ad52a5d10338d6f68e72db6a713487663b5
Petar Radosevic <petar@petar.dev>
2023-01-14 21:28:37 +0100

    feat: pick a random quote

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::seq::SliceRandom;
 use std::fmt::Display;
 
 /// The quote itself
@@ -11,6 +12,8 @@ impl Quote {
     fn parse(line: &str) -> Self {
         let mut parts = line.split("--");
 
+        // TODO: It should still fail to build when the string is invalid
+        // but the error message should be cleaner.
         let text = parts.next().unwrap().trim().to_owned();
         let author = parts.next().unwrap().trim().to_owned();
 
@@ -27,7 +30,11 @@ impl Display for Quote {
 fn main() {
     let raw = include_str!("quotes.txt");
     let quotes: Vec<Quote> = raw.lines().map(Quote::parse).collect();
-    println!("{}", quotes.first().unwrap());
+
+    let mut rng = rand::thread_rng();
+    let quote = quotes.choose(&mut rng).unwrap();
+
+    println!("{}", quote);
 }
 
 #[cfg(test)]
